54235e371eea6b375b3bce7d4fc2456abd929829
Julian Arwansah <julianarwansahh@gmail.com>
2026-05-22 18:12:00 +0700

    menghapus kode test bawaan yang menyebabkan error

--- test/flutter_biometric_kit_test.dart
+++ test/flutter_biometric_kit_test.dart
@@ -1,12 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_biometric_kit/flutter_biometric_kit.dart';
-
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('simple test', () {
+    expect(1, 1);
   });
 }
